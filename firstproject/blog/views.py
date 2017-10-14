b486f36c72711c0e2afadc6b6b89d35bd4c6de15
minekucukal <minekucukal@gmail.com>
2017-10-14 16:52:00 +0300

    post

--- firstproject/blog/views.py
+++ firstproject/blog/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
+from django.utils import timezone
+from .models import Post
 
-def post_list(request):     
-    return render(request, 'blog/post_list.html', {})
-    
-# Create your views here.
+def post_list(request):
+    posts = Post.objects.filter(yayinlamaTarihi__lte=timezone.now()).order_by('yayinlamaTarihi')
+    return render(request, 'blog/post_list.html', {'posts': posts})
